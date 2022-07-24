648106e335f39f52365026d8418b7b8b7f5a6985
oksanaPC2 <77opod@gmail.com>
2022-07-24 16:41:23 +0300

    Добавьте файлы проекта.

---
+++ №1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Get_S(double x, double y, double z, out double s)
+        {
+            double t;
+            t = (x + y + z)/2;
+            s= Math.Sqrt(t * (t - x) * (t - y) * (t - z));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача №1\n Два треугольника заданы длинами своих сторон. Определить, площадь какого из них больше.\n\nУ вас 10 попыток, удачи!");
+            int o = 1;
+            while (o < 11)
+            {
+                Console.WriteLine("\nПопытка №{0} из 10!", o);
+
+
+                Console.WriteLine("Введите длины первого треугольника (x,y,z):");
+                #region  ввод 
+                Console.WriteLine("Введите x:");
+                double x1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите y:");
+                double y1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите z:");
+                double z1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите длины второго треугольника (x,y,z):");
+                Console.WriteLine("Введите x:");
+                double x2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите y:");
+                double y2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите z:");
+                double z2 = Convert.ToDouble(Console.ReadLine());
+                double s1; double s2; double smax=0;
+                #endregion 
+
+                Get_S(x1, y1, z1, out s1);
+                Get_S(x2, y2, z2, out s2);
+                #region сравнение треугольников
+                if (s1 > s2)
+                {
+                    smax = s1;
+                    Console.WriteLine("S первого треугольника больше");
+                }
+                else
+                    if(s1<s2)
+                    {
+                    smax = s2;
+                    Console.WriteLine("S второго треугольника больше");
+                }
+                else
+                    if(s1 == s2)
+                    {
+                    Console.WriteLine("Площади равны");
+                }
+                #endregion
+                Console.WriteLine("\nS большего треугольника = {0}",smax);
+
+
+                o++;
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Cпасибо!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ №2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Get_V_S(double a, out double s, out double v)
+        {
+            s = Math.Pow(a, 2) * 6;
+            v = Math.Pow(a, 3);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача №2.Создать метод для вычисления объема и площади поверхности куба по длине его ребра. \n\nУ вас 10 попыток, удачи!");
+            int o = 1;
+            while (o < 11)
+            {
+                Console.WriteLine("\n\nПопытка №{0} из 10!", o);
+
+                #region решение
+                Console.WriteLine("Введите длину ребра куба");
+                double a = Convert.ToDouble(Console.ReadLine());
+                a = Math.Abs(a);
+                double s;
+                double v;
+                Get_V_S(a, out s, out v);
+                Console.WriteLine("{0:.0} {1:.0}", s, v);
+                #endregion
+
+                o++;
+            }
+        }
+    }
+}
